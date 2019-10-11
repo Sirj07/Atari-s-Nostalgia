0d45c5362ad4a6424dbcc9ace7285b59f98ed05b
JASON ANDREY <jason.andrey18@gmail.com>
2019-10-10 19:47:11 -0500

    Atari-s-Nostalgia 1
    listo  joven hay esta recuerde que sirve es con la version de unity o superior

---
+++ Supra/Atari´s Nostalgia/Assets/CarrosEnemigos.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarrosEnemigos : MonoBehaviour
+{
+    public GameObject car;
+    public Vector3[] positions = new Vector3[2];
+    void Start()
+    {
+        positions[0] = new Vector3(-1,7.5f);
+        positions[1] = new Vector3(2.86f,7.5f);
+
+        StartCoroutine(EnemyCreator());
+    }
+
+    IEnumerator EnemyCreator()
+    {
+        while (true)
+        {
+            Instantiate(car, positions[Random.Range(0,2)], new Quaternion(180, 0 ,0 , 180));
+            yield return new WaitForSeconds(3);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Supra/Atari´s Nostalgia/Assets/MovementCarro.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementCarro : MonoBehaviour
+{
+    
+    public Vector2 derechaX = new Vector2();    
+    public Vector2 izquierdaX = new Vector2();
+
+    void Start()
+    {
+        derechaX.x = transform.position.x + 3.9f;
+        izquierdaX.x = transform.position.x - -0f;
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D))
+            transform.position = new Vector2(derechaX.x,transform.position.y);
+        if(Input.GetKey(KeyCode.A))
+            transform.position = new Vector2(izquierdaX.x,transform.position.y);
+    }
+}
